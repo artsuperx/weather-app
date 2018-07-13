70e3412cfd938dcaba3947858ea6936170e5d496
artsuperx <art.nazarovx@gmail.com>
2018-07-13 17:57:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    forecast: [],
+    isFetching: false,
+    err: false
+  };
+
+  componentDidMount() {}
+
+  render() {
+    return <div>Wrapper</div>;
+  }
+}
+
+export default App;
