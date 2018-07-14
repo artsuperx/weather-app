7c84ec7474bda84d28a0adb0600be2147249d3f1
artsuperx <art.nazarovx@gmail.com>
2018-07-14 12:44:21 +0300

    feat: init data for Status component

--- src/Forecast/Weather/Status.js
+++ src/Forecast/Weather/Status.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import format from 'date-fns/format';
 
 export default ({ currentStatus }) => {
   console.log(currentStatus);
+  const { humidity, temp } = currentStatus.main;
+  const speed = currentStatus.wind.speed;
+  const date = format(currentStatus.dt_txt, 'dddd Do');
+  const icon = currentStatus.weather[0].icon;
+
   return <div>Status</div>;
 };
