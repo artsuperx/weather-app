bd8b797f74091b578daa209f381f4494f4cfd97d
artsuperx <art.nazarovx@gmail.com>
2018-07-14 12:15:43 +0300

    feat: init data for info component

--- src/Forecast/Info.js
+++ src/Forecast/Info.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import format from 'date-fns/format';
 
 export default ({ name, country, currentStatus }) => {
+  const currentTime = format(new Date(), 'hh:mm a');
+  const weather = currentStatus.weather[0];
+  const { description, icon } = weather;
+
   return <div>Info</div>;
 };
